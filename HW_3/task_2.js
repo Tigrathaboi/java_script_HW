fefdc406af2f296cf2b72efd23a8921df217c209
Tigran <47396710+Tigrathaboi@users.noreply.github.com>
2021-03-29 23:02:47 +0300

    Add files via upload

---
+++ HW_3/task_2.js
@@ -0,0 +1,8 @@
+function countProps(obj) {
+    let keys = Object.keys(obj);
+    return keys.length;
+}
+
+console.log(countProps({})); // 0
+console.log(countProps({ name: 'Mango', age: 2 })); // 2
+console.log(countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 })); // 3
